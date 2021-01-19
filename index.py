f1eb1cf2fb9fef37f5a61c763138389714e23831
charlie <sadegh.rez@gmail.com>
2021-01-19 22:29:48 +0330

    finish

--- index.py
+++ index.py
@@ -3,9 +3,13 @@ The prime factors of 13195 are 5, 7, 13 and 29.
 What is the largest prime factor of the number 600851475143  ?
 """
 
-pf = [2]
-goal = 20
+goal = 600851475143
 
+pf = [2] #prime factors
+mypf = [] #prime factors of goal
+
+if goal % 2 == 0 :
+	mypf.append(2)
 
 for x in range(3,goal) :
 	flag_pf = True
@@ -16,9 +20,13 @@ for x in range(3,goal) :
 	
 	if flag_pf :
 		pf.append(x)
+		if goal % x == 0 :
+			mypf.append(x)
 
 
-print (pf)
+#print (pf)
+#print (mypf)
+print (mypf[-1])
 
 			
 
